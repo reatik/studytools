269cbfe2c02b2d00b9e464ffe41a0edd7c0ca9ed
reatik <139283884+reatik@users.noreply.github.com>
2025-06-08 09:10:42 +0800

    a

--- subjects/english/spellgame/words.js
+++ subjects/english/spellgame/words.js
@@ -1,5 +1,8 @@
 const wordData = {
    words: [
+      {"english":"Children's Day","phonetic":"/ə; eɪ/","chinese":"一；一个","pos":"art.","unit":"M1U1","grade":"高一上","type":"word"},
+      {"english":"be born","phonetic":"/ə; eɪ/","chinese":"一；一个","pos":"art.","unit":"M1U1","grade":"高一上","type":"word"},
+      {"english":"(be) born","phonetic":"/ə; eɪ/","chinese":"一；一个","pos":"art.","unit":"M1U1","grade":"高一上","type":"word"},
 {"english":"a","phonetic":"/ə; eɪ/","chinese":"一；一个","pos":"art.","unit":"M1U1","grade":"三年级上","type":"word"},
 {"english":"after","phonetic":"/ˈɑːftə(r)/","chinese":"(时间) 在…… 后","pos":"prep.","unit":"M1U1","grade":"三年级上","type":"word"},
 {"english":"afternoon","phonetic":"/ˌɑːftəˈnuːn/","chinese":"下午","pos":"n.","unit":"M1U1","grade":"三年级上","type":"word"},
